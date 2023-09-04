b1adb4b92829aafa144c6871c8e4e0dbbfb4dd74
arunav321 <84830602+arunav321@users.noreply.github.com>
2023-09-04 14:50:16 +0530

    Update tt04_sahrdaya_lfsr.v

--- src/tt04_sahrdaya_lfsr.v
+++ src/tt04_sahrdaya_lfsr.v
@@ -1,9 +1,14 @@
 `default_nettype none
 
 module tt04_sahrdaya_lfsr (
-    input  wire [3:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [3:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire       clk,      // clock);
+     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n 
 );
 //lfsr4 uut (
 //    .clk(clk),
