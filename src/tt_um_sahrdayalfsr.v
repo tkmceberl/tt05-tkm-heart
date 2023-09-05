4e0d4fda4d1b77830de253be7bc5cc95e00f3907
arunav321 <84830602+arunav321@users.noreply.github.com>
2023-09-05 09:20:00 +0530

    Update and rename tt04_sahrdaya_lfsr.v to tt_um_sahrdayalfsr.v

--- src/tt_um_sahrdayalfsr.v
+++ src/tt_um_sahrdayalfsr.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt04_sahrdaya_lfsr (
+module tt_um_sahrdayalfsr (
      input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
