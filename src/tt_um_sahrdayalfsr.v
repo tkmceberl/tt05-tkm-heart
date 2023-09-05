5a9ea8015b338a591ed5dc6ee7113dc3e5ab0cc3
arunav321 <84830602+arunav321@users.noreply.github.com>
2023-09-05 09:32:01 +0530

    Update tt_um_sahrdayalfsr.v

--- src/tt_um_sahrdayalfsr.v
+++ src/tt_um_sahrdayalfsr.v
@@ -3,7 +3,7 @@
 module tt_um_sahrdayalfsr (
      input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-   // input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
   //  output wire [7:0] uio_out,  // IOs: Bidirectional Output path
   //  output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
