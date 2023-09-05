5f6ce8cc1233489fa58481425fe2d94c9979aa1b
arunav321 <84830602+arunav321@users.noreply.github.com>
2023-09-05 09:37:43 +0530

    Update tt_um_sahrdayalfsr.v

--- src/tt_um_sahrdayalfsr.v
+++ src/tt_um_sahrdayalfsr.v
@@ -4,12 +4,13 @@ module tt_um_sahrdayalfsr (
      input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-  //  output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-  //  output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n 
 );
+     assign uio_oe = 8'b11111111;
 //lfsr4 uut (
 //    .clk(clk),
 //    .reset(io_in[0]),
