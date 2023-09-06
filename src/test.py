f7b5c7194ae4102976c09cfa571fa0b121b03728
arunav321 <84830602+arunav321@users.noreply.github.com>
2023-09-06 10:05:44 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -4,14 +4,16 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_lfsr(dut):
     dut._log.info("start")
-    dut.INPUT.value = 1
-await Timer(1, units="ms")
-assert dut.OUTPUT.value == 0
-clock = Clock(dut.clk, 1, units="ms")
-cocotb.start_soon(clock.start())
-dut.rst_n.value = 0 # low to reset
-await ClockCycles(dut.clk, 10)
-dut.rst_n.value = 1 #take out of reset
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    # reset
+    dut._log.info("reset")
+    dut.rst_n.value = 0
+    # set the compare value
+    dut.ui_in.value = 1
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
 
 
       
