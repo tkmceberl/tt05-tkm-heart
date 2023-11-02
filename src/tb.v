9494d4567a818b9d46e7b75cafef276547fd832b
tkmceberl <149557263+tkmceberl@users.noreply.github.com>
2023-11-02 09:58:19 +0530

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_sahrdayalfsr tt_um_sahrdayalfsr (
+    tt_um_tkmheart tt_um_tkmheart (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
